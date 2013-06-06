d07781f40f7d14653b595e1690079632694966f6
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-06-06 16:56:35 -0400

    Fixed the Business controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -9,11 +9,8 @@ class BusinessesController < ApplicationController
 
   def create
     @user = User.find_by_remember_token(cookies[:remember_token])
-    @business = Business.new()
-    @business.Name = params[:Name]
-    @business.Phone = params[:Phone]
-    @business.Description = params[:Description]
-    @business.userId = @user
+    @business = Business.new(params[:business])
+     @business.userId = @user
     if @business.save
       flash[:success] = "Business Added"
       redirect_to @user

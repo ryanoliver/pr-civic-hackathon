3927df06194fa7ac82d1625bf55e65e90ec71756
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-06-06 16:51:55 -0400

    Added userId to business when new business is added

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -9,7 +9,11 @@ class BusinessesController < ApplicationController
 
   def create
     @user = User.find_by_remember_token(cookies[:remember_token])
-    @business = Business.new(params[:business])
+    @business = Business.new()
+    @business.Name = params[:Name]
+    @business.Phone = params[:Phone]
+    @business.Description = params[:Description]
+    @business.userId = @user
     if @business.save
       flash[:success] = "Business Added"
       redirect_to @user

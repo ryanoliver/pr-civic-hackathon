e0d606c2fa360ea3ec00566fe61ad2e76cd98049
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-06-06 13:11:24 -0400

    Added the helper module to the controller because the controller could not find the helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,67 @@
 class SessionsController < ApplicationController
 
+  def new 
+  end
+
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      sign_in user
+      sign_in(user)
       redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
-
+  
   def destroy
     sign_out
     redirect_to root_url
   end
+
+
+#This was supossed to be a helper
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
 end

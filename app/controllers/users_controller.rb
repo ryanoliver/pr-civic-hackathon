1663ae0c87409d03fae323753f50d9a25e66fea3
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-06-06 15:48:23 -0400

    When sign up user redirects to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome"
+      redirect_to @user
     else
       render 'new'
     end

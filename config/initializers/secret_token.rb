91e4a1094690dd325294d7138a2c424472ca13e6
r23712 <ryan.n.oliver@gmail.com>
2013-06-06 09:34:42 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CivicHack::Application.config.secret_token = 'd3a9b5440927c24dc5127310ab7877778e5f41f60ff6e364a1939f7b0654a6774f84e4f7abdefe1cf6c5d4d3b920565974b9f9a8b22e17665a8fce577a77d461'
